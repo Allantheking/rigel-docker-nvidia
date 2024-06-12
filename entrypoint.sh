11b33e12582f77dc34b7d6ea05431952ba4fe0b9
Allantheking <38585663+Allantheking@users.noreply.github.com>
2024-06-12 15:46:15 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,8 +16,8 @@ check_required_env "POOL"
 check_required_env "WALLET"
 
 
-./rigel -a "${ALGO}" -o "${POOL}" -u "${WALLET}" "${EXTRA}"
+./rigel --algorithm "${ALGO}" --url "${POOL}" --username "${WALLET}" "${EXTRA}"
 while [ $? -eq 42 ]; do
     sleep 15s
-    ./rigel -a "${ALGO}" -o "${POOL}" -u "${WALLET}" "${EXTRA}"
+    ./rigel --algorithm "${ALGO}" --url "${POOL}" --username "${WALLET}" "${EXTRA}"
 done
